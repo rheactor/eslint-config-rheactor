02cf1cf49ed5bd1dcf6a4f293b7fafe3effc1a56
David Rodrigues <david.proweb@gmail.com>
2023-12-13 21:02:07 -0300

    chore: update dependencies;

--- vitest.js
+++ vitest.js
@@ -6,11 +6,20 @@ module.exports = {
       files: "./**/*.test.ts",
       extends: ["plugin:vitest/all"],
       rules: {
-        "vitest/require-top-level-describe": "off",
-        "vitest/no-conditional-expect": "off",
         "vitest/max-expects": "off",
+        "vitest/no-conditional-expect": "off",
         "vitest/no-conditional-in-test": "off",
         "vitest/no-conditional-tests": "off",
+        "vitest/no-hooks": "off",
+        "vitest/prefer-expect-assertions": [
+          "warn",
+          {
+            onlyFunctionsWithAsyncKeyword: true,
+            onlyFunctionsWithExpectInLoop: true,
+            onlyFunctionsWithExpectInCallback: true,
+          },
+        ],
+        "vitest/require-top-level-describe": "off",
       },
     },
   ],
