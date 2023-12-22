8733db83f08a993ac648b136569bebb16e98461a
David Rodrigues <david.proweb@gmail.com>
2023-12-21 21:33:43 -0300

    chore: update dependencies;

--- unicorn.js
+++ unicorn.js
@@ -3,15 +3,16 @@ module.exports = {
   extends: ["plugin:unicorn/recommended"],
 
   rules: {
-    "unicorn/no-empty-file": "off", // bug
-    "unicorn/number-literal-case": "off", // prettier conflict
-    "unicorn/no-nested-ternary": "off", // prettier conflict
     "unicorn/better-regex": "off", // regexp conflict
-    "unicorn/no-null": "off",
+    "unicorn/catch-error-name": "off",
     "unicorn/filename-case": "off",
-    "unicorn/prevent-abbreviations": "off",
     "unicorn/no-await-expression-member": "off",
+    "unicorn/no-empty-file": "off", // bug
+    "unicorn/no-nested-ternary": "off", // prettier conflict
+    "unicorn/no-null": "off",
     "unicorn/no-useless-undefined": ["error", { checkArguments: false }],
+    "unicorn/number-literal-case": "off", // prettier conflict
+    "unicorn/prevent-abbreviations": "off",
   },
 
   overrides: [
